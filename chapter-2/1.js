ac1bffb288f22a0ab602cb4cd7a5250797e4db3b
Jatin <chaurasiyajatin68@gmail.com>
2024-01-23 22:19:59 +0530

    jatin

--- chapter-2/1.js
+++ chapter-2/1.js
@@ -2,6 +2,13 @@
 comments in js 
 part of code which is not executed
 */
+// this is code that prints 
+// console.log("hello world");
+
+
+
+
+
 
 
 
---
+++ chapter-2/2.js
@@ -0,0 +1,30 @@
+/*
+operators in js 
+
+used to perform some operations on data
+
+Arithmetic Operators
++,-,*,/
+
+
+*/
+let a = 5;
+let b = 2;
+let c = a+b;
+console.log("a+b = ", a+b);
+console.log("a-b = ", a-b);
+console.log("a*b = ", a*b);
+console.log("a/b = ", a/b);
+console.log("a%b = ", a%b);
+console.log("(a)^b = ", a**b);
+console.log("a+b = ", c);
+
+//unary operator
+a = a+1;
+console.log("a+1 = ", a);
+a++;// post addition
+console.log("a+2 = ", a);
+++a;//pre addition
+console.log("a+3= ", a);
+
+
