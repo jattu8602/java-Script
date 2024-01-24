e8cfa67b2c5a1fe17492803a4551b1cc9f3e56e2
Jatin <chaurasiyajatin68@gmail.com>
2024-01-24 17:46:48 +0530

    helllo

--- chapter-2/5.js
+++
@@ -1,9 +0,0 @@
-//         CONDITIONAL STATEMENTS
-// wap to determine that wheather given value is odd or even
-console.log("for determining that given value is odd or even");
-let num =15;
-if(num%2==0){
-    console.log("even");
-}else{
-    console.log("odd");
-}
