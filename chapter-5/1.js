8067e184a4ee5cf893437593d5a82802f7106340
Jatin <chaurasiyajatin68@gmail.com>
2024-02-08 21:57:27 +0530

    jatin

---
+++ chapter-5/1.js
@@ -0,0 +1,23 @@
+//                                 FUNCTION
+//block of code that performs a specific tasks ,can be invoked whenever needed
+
+//function definition
+// function functionName(){
+    //do some work
+// }
+
+//function call
+//functionName();
+// function functionName(param1.param2...){
+    //do some work
+// }
+function myFunction(){
+    console.log("welcome to apna College");
+    console.log("we are learning JS");
+    console.log("welcome to apna College");
+    console.log("we are learning JS");
+
+
+}
+myFunction();
+myFunction();//it helps to manage redundancy
