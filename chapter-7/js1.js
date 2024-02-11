4ab9a370954cbaab26e0839a66bc9d38a86af4c3
Jatin <chaurasiyajatin68@gmail.com>
2024-02-11 23:39:49 +0530

    jatin

---
+++ chapter-7/js1.js
@@ -0,0 +1,11 @@
+//                      DOM MANIPULATION
+// ATTRIBUTES
+
+// to get the attribute value
+// getAttribute(attr);
+
+// to set the sttribute value
+// setAttribute(attr,value);
+
+// STYLE
+// node.style
