5c18515e68987b920cea5c331400f38f69860ee9
Jatin <chaurasiyajatin68@gmail.com>
2024-02-11 09:56:02 +0530

    jatin

---
+++ chapter-6/1.js
@@ -0,0 +1,34 @@
+//                 DOM part - 1 
+//              starts
+//          The 3 MUSKETEERS of web dev
+// HTML                        CSS                          JS
+//(structure)                (style)                      (logic)
+
+
+
+
+
+
+
+
+//starter Code
+// <style> tag connects HTML with CSS
+// <script> tag connects HTML with JS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
