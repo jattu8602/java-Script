b8f6f6df15ecabc453cf02550b88c39088b81c0f
Jatin <chaurasiyajatin68@gmail.com>
2024-02-11 23:34:11 +0530

    jatin

--- chapter-6/q2.js
+++ chapter-6/q2.js
@@ -1,5 +1,19 @@
 // Create 3 divs with common class name - "box". Access them & add some unique text to each fo them
+let divs = document.querySelectorAll(".box");
+for(div of divs){
+    console.log(div.innerText);
+}
 
+let idx = 1;
+for(div of divs){
+    div.innerText = 'new value';
+    // div.innerText=idx;
+        idx++;
+}
+
+// divs[0].innerText = "new unique value 1";
+// divs[1].innerText = "new unique value 2";
+// divs[2].innerText = "new unique value 3";
 
 
 
