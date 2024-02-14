558e92f711a824e8e7b629d9cb5ee449ae7d26a1
Jatin <chaurasiyajatin68@gmail.com>
2024-02-14 22:39:55 +0530

    jaitn

--- chapter-8/2.js
+++ chapter-8/2.js
@@ -1,4 +1,19 @@
 //                      Event handling in JS
-Node.event = () => {
-    //handle here
+//             Node.event = () => {
+//                handle here
+//                                }
+let btn1 = document.querySelector('#btn1');
+btn1.onclick = () => {
+    console.log("btn1 was clicked;");
+    let a = 45;
+    a++;
+    console.log(a);
+}
+
+let div = document.querySelector("div");
+div.onmouseover = () =>{
+    console.log("You are inside the div");
+    let a= 34;
+    a--;
+    console.log(a);
 }
