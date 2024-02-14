10a337d5c36e31fcc86bf15d1a9440b699676e79
Jatin <chaurasiyajatin68@gmail.com>
2024-02-14 21:38:45 +0530

    jatin

---
+++ chapter-8/1.js
@@ -0,0 +1,9 @@
+///                               EVENTS IN JS 
+// The change in the state of an object is known as Event.
+// Events are fired to notify code of "intresting changes " that may affect code execution
+
+// Mouse events (click, double click, etc...)
+// Keyboard events (keypress, keyyup, keydown)
+// Form events (submit etc)
+// Print events & many more
+
