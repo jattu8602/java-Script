360629411a83159731f648888e6f72f59bf78231
Jatin <chaurasiyajatin68@gmail.com>
2024-02-18 07:37:07 +0530

    jatin

--- chapter-10/6.js
+++ chapter-10/6.js
@@ -9,7 +9,12 @@
 
 ///  * If Child & Parent have same method , child's method will be used .[Method Overriding]
 
-
+start(){
+    console.log("This is a start function.");
+}
+stop(){
+    console.log("This is a stop function.");
+}
 
 
 
