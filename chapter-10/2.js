e751a21b744340da6c786095fba429ce973ca21d
Jatin <chaurasiyajatin68@gmail.com>
2024-02-18 06:52:04 +0530

    jatin

--- chapter-10/2.js
+++ chapter-10/2.js
@@ -2,12 +2,34 @@ const employee = {
     calcTax1(){
         console.log("tax rate is 10%");
     },
-    calcTax2 : function(){
-        console.log("tax rate is 10%");
-    },
+    // calcTax2 : function(){
+    //     console.log("tax rate is 10%");
+    // },
     // both the methods of writing the calcTax1 & calcTax2 are same and correct
 };
 const karanArjun = {
     salary : 50000,
+    calcTax1(){
+        console.log("tax rate is 20%");
+    },
+    // **If object & prototype have same method, object's methods will be used.
+
 };
-karanArjun.__proto__ = employee
+const karanArjun1 = {
+    salary : 50000,
+};
+const karanArjun2 = {
+    salary : 50000,
+};
+const karanArjun3 = {
+    salary : 50000,
+};
+karanArjun.__proto__ = employee;
+
+karanArjun1.__proto__ = employee;
+
+karanArjun2.__proto__ = employee;
+
+karanArjun3.__proto__ = employee;
+
+//we dont need to give ecah karan arjun the function calc tax infact we can use prototype function to assign that work to them
---
+++ chapter-10/3.js
@@ -0,0 +1,9 @@
+//                 Classes in JS
+// class is a program-code template for creating objects.
+// Those objects will have some (variables) & some behaviours (functions) inside it.
+
+                                    // class MyClass{
+                                    //     constructor(){...}
+                                    //     myMethod(){...}
+                                    // }
+                                    // let myObj = new MyClass();
