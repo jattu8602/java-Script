7d55bad1650e49440d6f89c211cd23f2ddf78f84
Jatin <chaurasiyajatin68@gmail.com>
2024-02-23 21:47:45 +0530

    jatin

--- chapter-12/1.js
+++ chapter-12/1.js
@@ -1,2 +1,65 @@
-//                    FETCH API
-// 
+//                          FETCH API
+// The Fetch API provides an interface for fetching (sending/receiving) resources.
+// It uses Request and Response objects.
+// The fetch() methods is used to fetch a resource (data).
+
+//                 let promise = fetch(url,[option])
+
+const URL = "https://cat-fact.herokuapp.com/facts";
+let promise = fetch(URL);
+console.log(promise);
+
+const getFacts = async()=>{
+    console.log("getting data..........");
+    let response = await fetch(URL);
+    console.log(response.status);//json 
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ chapter-12/2.js
@@ -0,0 +1,4 @@
+//           Understanding Terms
+//  AJAX: is asynchronous JS & XML
+//  JSON: is javascript Object Notation       ------          AJAJ
+//  json() method: returns a second promise that resolves with the result of parsing the response body text as JSON . (input in JSON , output in JS object)
